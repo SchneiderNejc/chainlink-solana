eea9471770752855089ee9ecb22bb1fecf317156
Nejc Schneider <nejc.sch@gmail.com>
2024-06-26 11:54:46 +0200

    clean the boilerplate code

--- programs/chainlink_solana/src/lib.rs
+++ programs/chainlink_solana/src/lib.rs
@@ -6,10 +6,5 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod chainlink_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+}
