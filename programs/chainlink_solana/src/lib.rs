63a089bbd58c2df1a90cc1a42ca2c11aafb66b6d
Nejc Schneider <nejc.sch@gmail.com>
2024-06-26 12:02:06 +0200

    retrieve the latest rd of data

--- programs/chainlink_solana/src/lib.rs
+++ programs/chainlink_solana/src/lib.rs
@@ -6,6 +6,11 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod chainlink_solana {
     use super::*;
-
-
+    // Fetch the latest data from chainlink data feed
+    public fn execute(ctx: Context<Execute>) -> ProgramResult{
+        let round = chainlink::latest_round_data(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info())?;
+    }
 }
+
