7304a38ad0242144cc347f435b7ec7c647fd794d
Nejc Schneider <nejc.sch@gmail.com>
2024-06-26 12:47:09 +0200

    assign address for CHAINLINK_FEED and CHAINLINK_PROGRAM_ID

--- tests/chainlink_solana.ts
+++ tests/chainlink_solana.ts
@@ -1,5 +1,9 @@
 import * as anchor from "@coral-xyz/anchor";
 import { program } from "@coral-xyz/anchor/dist/cjs/native/system";
 
+// List of data feed account addresses: https://docs.chain.link/data-feeds/price-feeds/
+
+const CHAINLINK_FEED = "99B2bTijsU6f1GCT73HmdR7HCFFjGMBcPZY6jZ96ynrR"; // SOL -> USD @devnet
+const CHAINLINK_PROGRAM_ID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny";
 describe("chainlink_solana", () => {
 });
